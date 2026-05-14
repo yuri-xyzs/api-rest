b61ed98d123011869df360a4f05bb62d4ff8bd21
Thesoninho <thesoninho@gmail.com>
2026-05-13 21:02:25 -0300

    rota para alterar o usuário adicionada

--- server.js
+++ server.js
@@ -23,21 +23,31 @@ app.get("/clients/:id",
         const { id } = req.params
         const client = data.find( user => user.id == id)
         if (!client){
-            res.status(204).json()
+            res.status(204).json({result : 'User not found'})
         }
         res.json(client)
     }
 )
 
 app.post('/clients', function (req, res) {
-  const newUser = server_functions.createUser(req.body);
-  res.status(201).json(newUser);
+    const newUser = server_functions.createUser(req.body);
+    res.status(200).json(newUser);
 });
 
 app.put("/clients/id/:id",
     function(req,res){
-
+    const { id } = req.params
+    const  newUser  = req.body
+    console.log(`newUser: ${newUser}\n\n<- <-`)
+    const Older = data.find(user => user.id == id)
+    console.log(`Older: ${Older}\n\n<- <-`)
+    if (!Older){
+        res.status(204).json({result : 'User not found'})
     }
+    const result = server_functions.alterUser(Older, newUser)
+    res.status(200).json(result)
+    
+}
 )
 
 app.delete("/clients/id/:id",
